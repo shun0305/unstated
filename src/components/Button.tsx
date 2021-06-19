d455d8f18300eeaf968ee655618c8cc1bef3739a
shun0305 <shunbbra@icloud.com>
2021-06-19 17:30:40 +0900

    components作成

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button: React.FC = () => {
+  return (
+    <div>
+      <button>add!</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/components/Display.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Display: React.FC = () => {
+  return (
+    <div>
+      <h1>0</h1>
+    </div>
+  );
+};
+
+export default Display;
